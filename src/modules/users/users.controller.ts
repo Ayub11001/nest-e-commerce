30c39ad253b78c69d5c70db09cb7c4974c54fb2a
Ayub <sameerbaig690@gmail.com>
2026-04-11 17:42:51 +0530

    feat(users): add change password functionality and update user profile DTO

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { 
+    Body,
+    Controller, 
+    Delete, 
+    Get, 
+    HttpCode, 
+    HttpStatus, 
+    Param, 
+    Patch, 
+    Req, 
+    UseGuards
+ } from '@nestjs/common';
+import { 
+    ApiBearerAuth, 
+    ApiBody, 
+    ApiOperation, 
+    ApiResponse, 
+    ApiTags  
+ } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RoleGuard } from 'src/common/guards/roles.guard';
 import { UsersService } from './users.service';
 import { UserResponseDto } from './dto/user-response.dto';
 import type { RequestWithUser } from 'src/common/interfaces/request-with-user.interface';
+import { Roles } from 'src/common/decorators/role.decorator';
+import { Role } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
+import path from 'path';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @ApiTags('Users')
 @ApiBearerAuth('JWT-auth')
@@ -15,6 +38,7 @@ export class UsersController {
         private readonly usersService: UsersService
     ) {}
 
+    // Get a logged in user's profile
     @Get('me')
     @ApiOperation({
         summary: 'Get current logged in user profile'
@@ -30,5 +54,97 @@ export class UsersController {
     })
     async getProfile(@Req() req: RequestWithUser): Promise<UserResponseDto> {
         return await this.usersService.findOne(req.user.id);
+    }
+
+    // Get all the users
+    @Get()
+    @Roles(Role.ADMIN)
+    @ApiOperation({summary: 'Get all the users'})
+    @ApiResponse({
+        status: 200,
+        description: "A list of all the users",
+        type: [UserResponseDto]
+    })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async getAll(): Promise<UserResponseDto[]> {
+        return await this.usersService.findAll();
+    }
+
+    // Get user by id
+    @Get(':id')
+    @Roles(Role.ADMIN)
+    @ApiOperation({
+        summary: 'Get user by ID'
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'The user with specified ID',
+        type: UserResponseDto
+    })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    @ApiResponse({ status: 404, description: 'User not found' })
+    async getUserById(@Param('id') id: string): Promise<UserResponseDto> {
+        return await this.usersService.findOne(id);
+    }
+
+    // Update user info
+    @Patch('me')
+    @ApiOperation({ summary: 'Update current user profile' })
+    @ApiBody({ type: UpdateUserDto })
+    @ApiResponse({
+        status: 200,
+        description: 'The updated user profile',
+        type: UserResponseDto,
+    })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    @ApiResponse({ status: 409, description: 'Email already in use' })
+    async updateUser(
+        @GetUser('id') userId: string, 
+        @Body() updateUserDto: UpdateUserDto
+    ): Promise<UserResponseDto> {
+        return await this.usersService.update(userId, updateUserDto);
+    }
+
+    // Change user password
+    @Patch('me/password')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Change current user password' })
+    @ApiResponse({ status: 200, description: 'Password changed successfully' })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async changePassword(
+        @GetUser('id') userId: string, 
+        @Body() changePasswordDto: ChangePasswordDto,
+    ): Promise<{message: string}> {
+        return await this.usersService.changePassword(userId, changePasswordDto);
+    } 
+
+    // Delete user account
+    @Delete('me')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Delete current user account' })
+    @ApiResponse({
+        status: 200,
+        description: 'User account deleted successfully',
+    })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async deleteAccount(@GetUser('id') userId: string): 
+    Promise<{message: string}> {
+        return await this.usersService.remove(userId);
+    }
+    
+    // Delete user by ID (for the Admin)
+    @Delete(':id')
+    @Roles(Role.ADMIN)
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Delete user by ID' })
+    @ApiResponse({
+        status: 200,
+        description: 'User with the specified ID deleted successfully',
+    })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async deleteUserById(@Param('id') userID: string): 
+    Promise<{message: string}> {
+
+        return await this.usersService.remove(userID)
     }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserResponseDto } from './dto/user-response.dto';
-import { NotFoundError } from 'rxjs';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
+    private SALT_ROUNDS = 12
     constructor(
         private readonly prisma: PrismaService
     ) {}
@@ -21,7 +24,6 @@ export class UsersService {
                 role: true,
                 createdAt: true, 
                 updatedAt: true
-
             }
         });
         if(!user) {
@@ -30,4 +32,109 @@ export class UsersService {
 
         return user;
     }
+
+    async findAll(): Promise<UserResponseDto[]> {
+        return await this.prisma.user.findMany({
+            select: {
+                id: true, 
+                email: true, 
+                firstName: true,
+                lastName: true,
+                password: false,
+                role: true,
+                createdAt: true, 
+                updatedAt: true
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    }
+
+    async update(
+        userId: string, 
+        updateUserDto: UpdateUserDto
+    ): Promise<UserResponseDto> {
+        const existingUser = await this.prisma.user.findUnique({
+            where: {id: userId},
+        })
+        if(!existingUser) {
+            throw new NotFoundException('User not found')
+        }
+
+        if(updateUserDto.email && updateUserDto.email !== existingUser.email) {
+            const emailTaken = await this.prisma.user.findUnique({
+                where: {email: updateUserDto.email}
+            })
+            if(emailTaken) {
+                throw new NotFoundException('email already taken')
+            }
+        }
+
+        const updatedUser = await this.prisma.user.update({
+            where: {id: userId},
+            data: updateUserDto,
+            select: {
+                id: true, 
+                email: true, 
+                firstName: true,
+                lastName: true,
+                password: false,
+                role: true,
+                createdAt: true, 
+                updatedAt: true
+            }
+        });
+        return updatedUser;
+    }
+
+    async changePassword(
+        userId: string, 
+        changePasswordDto: ChangePasswordDto
+    ): Promise<{message: string}> {
+        const { currentPassword, newPassword } = changePasswordDto;
+        const user = await this.prisma.user.findUnique({
+            where: {id: userId}
+        });
+        if(!user) {
+            throw new NotFoundException('User not found')
+        }
+
+        const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
+        if(!isPasswordValid) {
+            throw new ForbiddenException('Incorrect Password')
+        }
+
+        const isSamePassword = await bcrypt.compare(newPassword, user.password);
+        if(isSamePassword) {
+            throw new ForbiddenException('New password must be different from th ecurrent one')
+        }
+
+        const newHashedPassword = await bcrypt.hash(newPassword, this.SALT_ROUNDS)
+
+        await this.prisma.user.update({
+            where: {id: userId},
+            data: {password: newHashedPassword}
+        });
+
+        return {
+            message: 'Password changed successfully'
+        }
+    }
+
+    async remove(userId: string): Promise<{message: string}>  {
+        const user = await this.prisma.user.findUnique({
+            where: {id: userId}
+        });
+        if(!user) {
+            throw new NotFoundException('User not found')
+        }
+
+        await this.prisma.user.delete({
+            where: {id: userId}
+        });
+
+        return {message: 'User account deleted successfully'}
+    }
+
 }
